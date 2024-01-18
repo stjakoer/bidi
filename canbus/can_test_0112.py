3e2e8a8d05d368b6fd7de5871771b14975ec62bf
Jannick <koerja01@thu.de>
2024-01-18 15:11:10 +0100

    Deleted

--- canbus/can_test_0112.py
+++ canbus/can_test_0112.py
@@ -5,15 +5,15 @@ import cantools
 def receive_can_messages(can_bus):
     # Lade die DBC-Datei
     database_dbc1 = cantools.db.load_file("ISC_CMS_Automotive.dbc")
+    for i in range(100):
+        # Empfange die nächste CAN-Nachricht
+        message = can_bus.recv()
+        if message is not None:
+            # Dekodiere die CAN-Nachricht mit Hilfe der DBC-Datei
+            decoded_message = database_dbc1.decode_message(message.arbitration_id, message.data)
 
-    # Empfange die nächste CAN-Nachricht
-    message = can_bus.recv()
-    if message is not None:
-        # Dekodiere die CAN-Nachricht mit Hilfe der DBC-Datei
-        decoded_message = database_dbc1.decode_message(message.arbitration_id, message.data)
-
-        # Gib die decodierte Nachricht aus
-        print(f"Received Message: {decoded_message}")
+            # Gib die decodierte Nachricht aus
+            print(f"Received Message: {decoded_message}")
 
 
 if __name__ == "__main__":
@@ -26,8 +26,8 @@ if __name__ == "__main__":
 
     # Gib die gewünschte Nachrichten-ID an
 
-    message_id = input("Bitte Message ID eingeben:")
-    print(type(message_id))
+    #message_id = input("Bitte Message ID eingeben:")
+    #print(type(message_id))
 
     # Rufe die Funktion für den CAN-Nachrichtenempfang auf
     receive_can_messages(canBus)

3e595286c57ed33ebc60c65665c5f058be98b145
Jannick <koerja01@thu.de>
2024-02-01 13:37:51 +0100

    Register des Plugs auslesen und in dictionary schreiben

--- evtec/EVTEC_Modbus.py
+++ evtec/EVTEC_Modbus.py
@@ -1,24 +1,48 @@
 from pyModbusTCP.client import ModbusClient
+import struct
 
-# IP-Adresse und Port des GO-E Charger
-host = "192.168.178.201"
-port = 5020
-unit_id = 1
+evtec = {}
+client = ModbusClient(host="192.168.178.201", port=5020, unit_id=2)
+slave_id = 2  # Fügen Sie weitere IDs hinzu, wenn Sie mehr Stecker haben
+register_addresses = [(0,1,'State'),
+                      (1,1,'ChargeState'),
+                      (2,1,'SessionType'),
+                      (3,2,'Voltage'),
+                      (5,2,'PowerUInt'),
+                      (7,2,'Current'),
+                      (9,2,'Power'),
+                      (11,1,'SOC'),
+                      (12,1,'ConnectorType'),
+                      (17,2,'ChargeTime'),
+                      (19,2,'ChargedEnergy'),
+                      (21,2,'DischargedEnergy'),
+                      (55,4,'Error'),
+                      (59,2,'TotalBatteryCapacity'),
+                      (61,2,'RemainingBatteryCapacity'),
+                      (63,2,'MinimalBatteryCapacity'),
+                      (65,2,'BulkChargeCapacity'),
+                      (120,12,'EVCC ID')]     # (register,länge,name)
 
-# Erstelle eine Modbus-Client-Verbindung
-c = ModbusClient(host=host, port=port, unit_id=unit_id, auto_open=True)
+client.open()
 
-regs = c.read_holding_registers(12, 1)
+for address, length, name in register_addresses:
+    regs = client.read_holding_registers(address, length)
+    if regs:
+        if length == 1:
+            value = regs[0]
+            print(f"{address:}: {value}")
+        elif length == 2:
+            value = struct.unpack('>f', struct.pack('>HH',*regs))[0]
+            print(f"{address}-{address+1}: {value}")
+        elif length == 4:
+            value = struct.unpack('>d', struct.pack('>HHHH', *regs))[0]
+        elif length == 12:
+            values = [struct.unpack('>f', struct.pack('>HH', regs[i], regs[i+1]))[0] for i in range(0, 12, 2)]
+    else:
+        print(f"Fehler beim Lesen des Registers {address}")
+    evtec[address] = {'name': name, 'value': value}
 
-if regs:
-    print(regs)
-    print(len(regs))
-    print(type(regs[0]))
-else:
-    print("read error")
+# Schließen Sie die Verbindung
+client.close()
 
-if c.write_multiple_registers(202,[0]):
-    print("write ok")
-else:from pyModbusTCP.client import ModbusClient
-    print("write error")
-
+print(evtec.items())

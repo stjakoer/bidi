77eb70a6bfb5e5515812027a5be8df71e62b30a6
Jannick <koerja01@thu.de>
2024-02-01 19:55:30 +0100

    error codes hinzugefügt

--- GUI_Bidi/CINERGIA_Modbus.py
+++ GUI_Bidi/CINERGIA_Modbus.py
@@ -8,6 +8,7 @@ def cinergia_modbus():
                           (13002, 2, 'Alarm_ABR_2', 'int'),
                           (13004, 2, 'Alarm_ABR_3', 'int'),
                           (13006, 2, 'Alarm_ABR_4', 'int'),
+                          (13008, 2, 'Alarm_ABR_5', 'int'),
                           (16000, 2, 'SW_GrafecetState', 'int'),
                           (16006, 2, 'SW_AC_DC_Selector_U', 'int'),
                           (16012, 2, 'SW_GE_EL_Selector', 'int'),
@@ -15,6 +16,11 @@ def cinergia_modbus():
                           (16018, 2, 'SW_Bipolar', 'int'),
                           (16020, 2, 'SW_BranchControl', 'int'),
                           (16022, 2, 'SW_ControlOperationU', 'int'),
+                          (23000, 2, 'Alarm_INV_1', 'int'),
+                          (23002, 2, 'Alarm_INV_2', 'int'),
+                          (23004, 2, 'Alarm_INV_3', 'int'),
+                          (23006, 2, 'Alarm_INV_4', 'int'),
+                          (23008, 2, 'Alarm_INV_5', 'int'),
                           (26094, 2, 'Voltage_Output_U_RMS', 'float'),
                           (26106, 2, 'Current_Output_Global', 'float'),
                           (26120, 2, 'Power_Active_Output_Total', 'float')]
@@ -39,6 +45,96 @@ def cinergia_modbus():
         cinergia[address] = {'name': name, 'value': value}
 
     def description(cinergia_new):
+        alarm_dict = {
+            0: "No alarm",
+            100: "Watchdog",
+            101: "Node without HB DSP",
+            102: "SPI_DSP_CRC",
+            103: "Node without HB BBB",
+            104: "Wrong DSP Loaded Prog",
+            110: "Device not initialized",
+            111: "DO Version Error",
+            112: "EEPROM Blank",
+            120: "INV Alarmed",
+            121: "ABR Alarmed",
+            130: "Node_Slave_2",
+            131: "Node_Slave_3",
+            132: "Node_Slave_4",
+            133: "Node_Slave_5",
+            134: "Node_Slave_6",
+            135: "Node_Slave_7",
+            136: "Node_Slave_8",
+            137: "Node_Slave_9",
+            138: "Node_Slave_10",
+            139: "Slave Alarmed",
+            140: "Wrong Paralel Maximum Config",
+            141: "Paralel_Master_HB",
+            142: "Paralel_Slave_HB",
+            143: "Paralel_Master_Config_Sl",
+            144: "Paralel_CRC_Coms",
+            145: "Paralel_Slave_Out_Switch",
+            146: "Paralel_Slave_wrong_mode",
+            150: "Wrong_AC_DC_Config",
+            151: "Wrong_1_Ch_Config",
+            152: "Wrong_Unipolar_Config",
+            153: "Wrong_Delta_Connection",
+            154: "Running_Config_Modif",
+            170: "Serial_Master_HB",
+            171: "Serial_CRC_Coms",
+            172: "Serial_Slave_HB",
+            173: "Wrong_UnitSerial_AC",
+            174: "Wrong_UnitSerial_3Ch",
+            175: "Wrong_UnitSerial_Bipolar",
+            176: "Serial_Slave_Out_Switch",
+            201: "Drivers Ph1",
+            202: "Drivers Ph2",
+            203: "Drivers Ph3",
+            211: "Heatsink temperature ABR",
+            212: "Heatsink temperature INV",
+            213: "Room Temperature",
+            221: "Fast precharge",
+            222: "Overload precharge",
+            230: "Output Contactor U",
+            231: "Output Contactor V",
+            232: "Output Contactor W",
+            301: "DC-Link Over Voltage",
+            302: "DC-Link Under Voltage",
+            310: "Overvoltage Main Grid",
+            311: "Overvoltage 1",
+            312: "Overvoltage 2",
+            313: "Overvoltage 3",
+            314: "Overvoltage Peak 1",
+            315: "Overvoltage Peak 2",
+            316: "Overvoltage Peak 3",
+            317: "Undervoltage Main Grid",
+            318: "Undervoltage 1",
+            319: "Undervoltage 2",
+            320: "Undervoltage 3",
+            330: "Overcurrent MainGrid",
+            331: "Overcurrent 1",
+            332: "Overcurrent 2",
+            333: "Overcurrent 3",
+            334: "Overcurrent Peak 1",
+            335: "Overcurrent Peak 2",
+            336: "Overcurrent Peak 3",
+            337: "Overcurrent Capacitor 1",
+            338: "Overcurrent Capacitor 2",
+            339: "Overcurrent Capacitor 3",
+            340: "Overcurrent Neutral",
+            350: "Overload MainGrid",
+            351: "Overload 1",
+            352: "Overload 2",
+            353: "Overload 3",
+            1001: "Emergency_Sequence",
+            1010: "Mains Lost",
+            1011: "Isolation Device"
+        }  # alarm description/error code
+        # register3000,13002,13004,13006,13006 & 23000,23002,23004,23006,23008
+        for register in [13000, 13002, 13004, 13006, 13006, 23000, 23002, 23004, 23006, 23008]:
+            for key in alarm_dict.keys():
+                if cinergia_new[register]['value'] == key:
+                    cinergia_new[register]['dev'] = alarm_dict[key]
+
         # register16000
         var_temp = cinergia_new[16000]['value']
         if var_temp == 2:
@@ -86,14 +182,14 @@ def cinergia_modbus():
 
     print(description(cinergia))
 
+
 cinergia_modbus()
 
 
-def schreiben(register, value)
-    dkfaslfödk
+def schreiben(register, value):
+    dkfaslfdk = 8
 
-    return
-
+    return dkfaslfdk
 
 
 schreiben(1703, 0)

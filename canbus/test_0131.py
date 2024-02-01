12937eb350a87a51cdfc3dcb10b760f884a42681
Jannick <koerja01@thu.de>
2024-02-01 11:40:14 +0100

    Möglichkeit alle botschaften auszulesen

--- canbus/test_0131.py
+++ canbus/test_0131.py
@@ -10,13 +10,10 @@ def can_bus_reader(bus, db):
                 # Finden Sie die entsprechende CAN-Nachricht in der DBC-Datei.
                 can_message = db.get_message_by_frame_id(message.arbitration_id)
 
-                # Überprüfen Sie, ob die Nachricht die gesuchte Nachricht ist.
-                if can_message.name == 'EVSEDCMaxLimits':
-                    # Dekodieren Sie die CAN-Nachricht.
-                    decoded_data = can_message.decode(message.data)
+                # Dekodieren Sie die CAN-Nachricht.
+                decoded_data = can_message.decode(message.data)
 
-                    print(f"ID: {message.arbitration_id}, Daten: {decoded_data}")
-                    break  # Beenden Sie die Schleife nach dem Lesen der gesuchten Nachricht.
+                print(f"ID: {message.arbitration_id}, Daten: {decoded_data}")
             except KeyError:
                 print(f"Unbekannte Nachricht ID: {message.arbitration_id}")
 

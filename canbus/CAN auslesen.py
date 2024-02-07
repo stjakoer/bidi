dc498e4743688b63770170b921807ae0835e480a
Jannick <koerja01@thu.de>
2024-02-07 15:46:14 +0100

    Nachricht decoden

--- canbus/CAN auslesen.py
+++ canbus/CAN auslesen.py
@@ -6,7 +6,8 @@ import cantools
 database = cantools.database.load_file("ISC_CMS_Automotive.dbc")
 
 # CAN-Nachricht
-can_message = can.Message(arbitration_id=8193, data=[0x02, 0x03, 0x04, 0x02, 0x00, 0x00, 0x00, 0x00])
+can_message = can.Message(arbitration_id=8194, data=[0x31, 0x00, 0x01, 0x00, 0x02, 0x14, 0x00, 0x00])
+# ID=8194x,Type=D,Length=8,Data=310001000214
 
 
 # Dekodiere

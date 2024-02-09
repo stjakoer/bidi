6bf3fec1275cbd86078872e3f32a52780036c5d3
Jannick <koerja01@thu.de>
2024-02-09 15:57:41 +0100

    zeit zum canbus abfragen verlängert, sodass alle botschaften empfangen werden

--- GUI_Bidi/CMS.py
+++ GUI_Bidi/CMS.py
@@ -1,6 +1,11 @@
+import time
+
+import can
+import cantools
+cms_read_dict = {}
+
 def cms_read():
-    import can
-    import cantools
+    global cms_read_dict
     cms_read_dict = {
         "Linkstatus": None,
         "ErrorCodeLevel3": None,
@@ -8,14 +13,14 @@ def cms_read():
         "ErrorCodeLevel1": None,
         "ErrorCodeLevel0": None,
         "EVSEEnergyTransferType": None,
-        "AliveCounter": None,
-        "VoltageMatch": None,
-        "ControlPilotState": None,
-        "ControlPilotDutyCiycle": None,
+        "AliveCounter": None,   #
+        "VoltageMatch": None,   #
+        "ControlPilotState": None,  #
+        "ControlPilotDutyCycle": None,  #
         "TCPStatus": None,
-        "ActualChargeProtocol": None,
-        "ProximityPinState": None,
-        "StateMachineState": None,
+        "ActualChargeProtocol": None,   #
+        "ProximityPinState": None,  #
+        "StateMachineState": None,  #
         "EVSENotificationMaxDelay": None,
         "EVSENotification": None,
         "EVSEStatusCode": None,
@@ -34,22 +39,22 @@ def cms_read():
     print(cms_read_dict)
     canBus = can.interface.Bus(bustype="pcan", channel="PCAN_USBBUS1", bitrate="500000")
     database_dbc = cantools.db.load_file("ISC_CMS_Automotive.dbc")
+    start_time = time.time()
+    while time.time() - start_time < 0.15:
+        raw_message = canBus.recv()
+        if raw_message is not None:
+            coded_message = database_dbc.get_message_by_frame_id(raw_message.arbitration_id)
+            decoded_botschaft = coded_message.decode(raw_message.data)
+            print("Decoded message: ", decoded_botschaft)
 
-    raw_message = canBus.recv()
+            for keys_dict in cms_read_dict.keys():
+                for key_bot in decoded_botschaft.keys():
+                    if keys_dict == key_bot:
+                        cms_read_dict[keys_dict] = decoded_botschaft[key_bot]
+
     canBus.shutdown()
-    if raw_message is not None:
-        coded_message = database_dbc.get_message_by_frame_id(raw_message.arbitration_id)
-        decoded_botschaft = coded_message.decode(raw_message.data)
-
-        for keys_dict in cms_read_dict.keys():
-            for key_bot in decoded_botschaft.keys():
-                if keys_dict == key_bot:
-                    cms_read_dict[keys_dict] = decoded_botschaft[key_bot]
 
     return cms_read_dict
-
-
-
 
 
 # cms_write_dict = {
@@ -67,23 +72,16 @@ def cms_read():
 #     "EVMaxCurrent": None
 # }
 
-
 def cms_write(botschaft, signal, value):
-    import can
-    import cantools
-
     canBus = can.interface.Bus(bustype="pcan", channel="PCAN_USBBUS1", bitrate="500000")
     database_dbc = cantools.db.load_file("ISC_CMS_Automotive.dbc")
     tester = cantools.tester.Tester('CMS', database_dbc, canBus, 'ISC_CMS_Automotive')
 
     tester.messages[botschaft][signal] = value
 
-
 def cms_userguide():
 
     cms_write('EVStatusControl', 'BCBControl', 'Stop')
 
     print(cms_read())
-
-
-cms_userguide()
+    return

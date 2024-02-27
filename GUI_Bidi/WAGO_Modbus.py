e207f95400f05ca31f51cc2a725c746ca9fa28dc
Jannick <koerja01@thu.de>
2024-02-27 21:41:06 +0100

    Funktionsstruktur angelegt um variablen zu lesen/schreiben

--- GUI_Bidi/WAGO_Modbus.py
+++ GUI_Bidi/WAGO_Modbus.py
@@ -1,16 +1,45 @@
 from pyModbusTCP.client import ModbusClient
-
 client = ModbusClient(host="192.168.178.202", port=502)
-regs = None
 
-if client.open():
-    regs = client.read_input_registers(0, 1)
-    print(regs)
-else:
-    print("Keine Verbindung zur Wago")
+wago_dict = {'status': {'value': None, 'reg-addr': 0},  # Abfrage ob GUI Starten darf
+             'contactor_state': {'value': None, 'reg-addr': 1}, # Ob die Schütze wirklich zu sind
+             }
 
-if client.write_multiple_registers(1, [1]): # immer Array übergeben!
-    print("Erfolgreich")
-else:
-    print("Fehler")
+"""So kann das Wago Dictionary aufgebaut werden. Ich denke, dass diese beiden Variablen aber reichen
+sollten. Einmal der Status, ob alles OK ist und die GUI starten darf (0 = nicht starten, 1 = starten,
+2 = laden abbrechen) und einmal, dass die Schütze
+wirklich zu sind, damit der Ladevorgang weiter voranschreiten kann"""
 
+
+def wago_modbus():
+    global wago_dict
+
+    if client.open():
+        j = 0
+        for keys in wago_dict:
+            wago_dict[keys]['value'] = client.read_input_registers(j, 1) # j = register & 1 entspricht die Breite
+            j += 1
+    else:
+        print("Keine Verbindung zur Wago!")
+
+
+wago_write_dict = {'close_contactor': 0,
+                   'open_contactor': 1}
+
+
+def wago_write_modbus(write_name, write_value):
+    for keys in wago_write_dict:
+        if keys == write_name:
+            if client.write_multiple_registers(wago_write_dict[keys], [write_value]):
+                # Abfrage ob schreiben erfolgreich war
+                print("Erfolgreich")
+            else:
+                print("Fehler beim schreiben der Wago")
+
+
+def main():
+    wago_modbus()
+
+
+if __name__ == "__main__":
+    main()

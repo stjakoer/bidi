8f5da4a5d27658a1af4f4285920c74afcf3d14a2
Jannick <koerja01@thu.de>
2024-03-18 00:22:29 -0400

    nur kopiert

---
+++ GUI_Bidi_V2/connect/connect_cms.py
@@ -0,0 +1,199 @@
+import time
+import can
+import cantools
+cms_read_dict = {
+    # "Linkstatus": None,
+    # "ErrorCodeLevel3": None,
+    # "ErrorCodeLevel2": None,
+    # "ErrorCodeLevel1": None,
+    # "ErrorCodeLevel0": None,
+    # "EVSEEnergyTransferType": None,
+    # "AliveCounter": None,   #
+    "VoltageMatch": None,  #
+    "ControlPilotState": None,  #
+    "ControlPilotDutyCycle": None,  #
+    "TCPStatus": None,
+    # "ActualChargeProtocol": None,   #
+    "ProximityPinState": None,  #
+    "StateMachineState": None,  #
+    # "EVSENotificationMaxDelay": None,
+    # "EVSENotification": None,
+    # "EVSEStatusCode": None,
+    # "EVSEPowerLimitAchieved": None,
+    # "EVSEVoltageLimitAchieved": None,
+    # "EVSECurrentLimitAchieved": None,
+    "EVSEIsolationStatus": None,
+    "EVSEPresentVoltage": None,
+    # "EVSECurrentRegulationTolerance": None,
+    # "EVSEPeakCurrentRipple": None,
+    "EVSEMinVoltage": None,
+    "EVSEMaxCurrent": None
+}
+
+EVMaxCurrent = 25
+EVMaxVoltage = 450
+EVMaxPower = 450*50
+
+try:
+    canBus = can.interface.Bus(bustype="pcan", channel="PCAN_USBBUS1", bitrate="500000")
+    database_dbc = cantools.db.load_file("ISC_CMS_Automotive.dbc")
+    can_tester = cantools.tester.Tester('CMS', database_dbc, canBus, 'ISC_CMS_Automotive')
+    canConnection = True
+except Exception as e:
+    print('Can\'t connect to CMS')
+    canConnection = False
+
+
+def cms_canbus_listener():
+    global cms_read_dict
+    global canConnection
+    if canConnection:   # Abfrage ob der Bus eine Verbindung hat
+        start_time = time.time()
+        while time.time() - start_time < 0.15:
+            raw_message = canBus.recv()
+            if raw_message is not None:
+                coded_message = database_dbc.get_message_by_frame_id(raw_message.arbitration_id)
+                decoded_botschaft = coded_message.decode(raw_message.data)
+
+                for keys_dict in cms_read_dict.keys():
+                    for key_bot in decoded_botschaft.keys():
+                        if keys_dict == key_bot:
+                            cms_read_dict[keys_dict] = decoded_botschaft[key_bot]
+    else:
+        print('Can\'t connect to CMS')
+
+
+def cms_read_dict_handover():
+    global cms_read_dict
+    # cms_canbus_listener() sollte nicht nötig sein, da die funktion im thread läuft. Testen!!
+    return cms_read_dict
+
+
+def precharge_cms(evcurrent, evvoltage):
+    evsoc = 54
+    can_tester.start()
+
+    can_tester.messages['EVStatusControl']['BCBControl'] = 'Stop'
+    can_tester.messages['EVStatusControl']['ChargeProgressIndication'] = 'Stop'
+    can_tester.messages['EVStatusControl']['ChargeProtocolPriority'] = 'DIN_only'
+    can_tester.messages['EVStatusControl']['ChargeStopIndication'] = 'NoStop'
+    can_tester.messages['EVStatusControl']['EVReady'] = 'False'
+    can_tester.messages['EVStatusControl']['EVWeldingDetectionEnable'] = 'False'
+
+    can_tester.messages['EVPlugStatus']['EVACReqStateC'] = 'SNA'
+    can_tester.messages['EVPlugStatus']['EVProximityPinState'] = 'SNA'
+    can_tester.messages['EVPlugStatus']['EVControlPilotState'] = 'SNA'
+    can_tester.messages['EVPlugStatus']['EVControlPilotDutyCycle'] = 'SNA'
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', timeout=5) is not None, "CME dead?"
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Default'},
+                             timeout=3) is not None, "CME not unplugged?"
+
+    print('Please plug in...')
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'ControlPilotState': 'B'}), "Didn't detect plug-in within 10s"
+    print('Pilot State B detected')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Init'}), "No HLC?"
+    print('Charge Init')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Authentication'})
+    print('Authentication')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.messages['EVDCMaxLimits']['EVMaxCurrent'] = EVMaxCurrent
+    can_tester.messages['EVDCMaxLimits']['EVMaxVoltage'] = EVMaxVoltage
+    can_tester.messages['EVDCMaxLimits']['EVMaxPower'] = EVMaxPower
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Parameter'})
+    print('Parameter')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.messages['EVStatusDisplay']['EVErrorCode'] = 0
+    can_tester.messages['EVStatusDisplay']['EVSoC'] = evsoc
+    can_tester.messages['EVStatusControl']['EVReady'] = 'True'
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Isolation'}, timeout=20)
+    print('Isolation')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.messages['EVDCChargeTargets']['EVPreChargeVoltage'] = evvoltage
+    can_tester.messages['EVDCChargeTargets']['EVTargetVoltage'] = evvoltage
+    can_tester.messages['EVDCChargeTargets']['EVTargetCurrent'] = evcurrent
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'PreCharge'}, timeout=10)
+    print('PreCharge')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    cnt = 0
+    matched = False
+    can_tester.flush_input()
+    while not matched:
+        charge_info = can_tester.expect('ChargeInfo')
+        if 'VoltageMatch' in charge_info and charge_info['VoltageMatch'] == 'True':
+            matched = True
+        charge_targets = can_tester.expect('EVSEDCStatus')
+        current = charge_targets['EVSEPresentCurrent']
+        voltage = charge_targets['EVSEPresentVoltage']
+        if voltage == 'SNA':
+            voltage = 0.0
+        if current == 'SNA':
+            current = 0.0
+        print("%10d sec Voltage: %f, Current: %f" % (cnt, voltage, current))
+        time.sleep(0.1)
+        cnt += 1
+        can_tester.flush_input()
+
+    can_tester.stop()
+
+
+def start_charging_cms():
+    can_tester.start()
+    can_tester.messages['EVStatusControl']['ChargeProgressIndication'] = 'Start'
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'Charge'}, timeout=3)
+    print('Charge')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    can_tester.stop()
+
+def stop_charging_cms():
+    can_tester.start()
+    can_tester.messages['EVStatusControl']['ChargeProgressIndication'] = 'Stop'
+
+    can_tester.messages['EVDCChargeTargets']['EVTargetVoltage'] = 0
+    can_tester.messages['EVDCChargeTargets']['EVTargetCurrent'] = 0
+
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'StateMachineState': 'ShutOff'})
+    print('ShutOff')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    print('Waiting for Unplug')
+    can_tester.flush_input()
+    assert can_tester.expect('ChargeInfo', {'ControlPilotState': 'E'})
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    can_tester.stop()
+
+
+def main():
+    precharge_cms(10, 350)
+    time.sleep(0.5)
+    start_charging_cms()
+    time.sleep(10)
+    stop_charging_cms()
+
+
+if __name__ == "__main__":
+    main()
---
+++ GUI_Bidi_V2/connect/connect_evtec.py
@@ -0,0 +1,121 @@
+def evtec_modbus():
+    from pyModbusTCP.client import ModbusClient
+    import struct
+
+    def description(evtec_new):
+        # register 0
+        var_temp = evtec_new[0]['value']
+        if var_temp == 0:
+            evtec_new[0]['def'] = "Unavailable"
+        elif var_temp == 1:
+            evtec_new[0]['def'] = "Available"
+        elif var_temp == 2:
+            evtec_new[0]['def'] = "Occupied"
+        elif var_temp == 3:
+            evtec_new[0]['def'] = "Preparing"
+        elif var_temp == 4:
+            evtec_new[0]['def'] = "Charging"
+        elif var_temp == 5:
+            evtec_new[0]['def'] = "Finishing"
+        elif var_temp == 6:
+            evtec_new[0]['def'] = "Suspended EV"
+        elif var_temp == 7:
+            evtec_new[0]['def'] = "Suspended EVSE"
+        elif var_temp == 8:
+            evtec_new[0]['def'] = "Not ready"
+        elif var_temp == 9:
+            evtec_new[0]['def'] = "Faulted"
+        else:
+            evtec_new[0]['def'] = "Couldnt do def!"
+
+        # register 1
+        var_temp = evtec_new[1]['value']
+        if var_temp == 0:
+            evtec_new[1]['def'] = "Charging process not started \n(no vehicle connected)"
+        elif var_temp == 1:
+            evtec_new[1]['def'] = "Connected, waiting for release \n(by RFID or Local)"
+        elif var_temp == 2:
+            evtec_new[1]['def'] = "Charging process starts"
+        elif var_temp == 3:
+            evtec_new[1]['def'] = "Shop"
+        elif var_temp == 4:
+            evtec_new[1]['def'] = "Suspended (loading paused)"
+        elif var_temp == 5:
+            evtec_new[1]['def'] = "Charging process successfully \ncompleted (vehicle still plugged in)"
+        elif var_temp == 6:
+            evtec_new[1]['def'] = "Charging process completed by \nuser (vehicle still plugged in)"
+        elif var_temp == 7:
+            evtec_new[1]['def'] = "Charging ended with error \n(vehicle still connected)"
+        else:
+            evtec_new[1]['def'] = "Couldn't do def!"
+
+        # register 3
+        evtec_new[3]['def'] = "Actual Output Voltage (DC)"
+        # register 5
+        evtec_new[5]['def'] = "Actual Output Power (Unsigned)"
+        # register 7
+        evtec_new[7]['def'] = "Actual Output Current (DC)"
+        # register 9
+        evtec_new[9]['def'] = "Actual OUtput Power"
+        # register 12
+        var_temp = evtec_new[12]['value']
+        if var_temp == 0:
+            evtec_new[12]['def'] = "AC"
+        elif var_temp == 1:
+            evtec_new[12]['def'] = "CCS"
+        elif var_temp == 2:
+            evtec_new[12]['def'] = "CHAdeMO"
+        elif var_temp == 3:
+            evtec_new[12]['def'] = "GBT"
+        else:
+            evtec_new[12]['def'] = "Couldnt do def!"
+
+        return evtec_new
+
+    evtec = {}
+    client = ModbusClient(host="192.168.178.201", port=5020, unit_id=2)
+    register_addresses = [(0, 1, 'State'),
+                          (1, 1, 'ChargeState'),
+                          (2, 1, 'SessionType'),
+                          (3, 2, 'Voltage'),
+                          (5, 2, 'PowerUInt'),
+                          (7, 2, 'Current'),
+                          (9, 2, 'Power'),
+                          (11, 1, 'SOC'),
+                          (12, 1, 'ConnectorType'),
+                          (17, 2, 'ChargeTime'),
+                          (19, 2, 'ChargedEnergy'),
+                          # (21, 2, 'DischargedEnergy'),
+                          (55, 4, 'Error'),
+                          # (59, 2, 'TotalBatteryCapacity'),
+                          # (61, 2, 'RemainingBatteryCapacity'),
+                          # (63, 2, 'MinimalBatteryCapacity'),
+                          # (65, 2, 'BulkChargeCapacity'),
+                          # (120, 12, 'EVCC ID')
+                          ]  # (register,länge,name)
+
+    if client.open():
+        for address, length, name in register_addresses:
+            regs = client.read_holding_registers(address, length)
+            value = regs
+            if regs:
+                if length == 1:
+                    value = regs[0]
+                elif length == 2:
+                    value = struct.unpack('>f', struct.pack('>HH', *regs))[0]
+                elif length == 4:
+                    value = struct.unpack('>d', struct.pack('>HHHH', *regs))[0]
+                elif length == 12:
+                    value = [struct.unpack('>f', struct.pack('>HH', regs[i], regs[i + 1]))[0] for i in range(0, 12, 2)]
+            else:
+                print(f"EVTEC: Fehler beim Lesen des Registers {address}")
+            evtec[address] = {'name': name, 'value': value}
+
+        client.close()
+
+        evtec = description(evtec)
+    else:
+        print("Verbindung zur EVTEC konnte nicht hergestellt werden!")
+
+    return evtec
+

ba3614cf654b8be3c8cc32bb2aa65611f5ee34b6
Jannick <koerja01@thu.de>
2024-08-15 09:47:37 +0200

    print hinzugefügt

--- GUI_Bidi/GUI_Bidi.py
+++ GUI_Bidi/GUI_Bidi.py
@@ -340,6 +340,7 @@ def stop_charging():
         if cms_dict['StateMachineState'] == 'ShutOff' and wago_dict['dcminus_contactor_state_open']['value'] == 1 and wago_dict['dcplus_contactor_state_open']['value'] == 0:
             wago_write_modbus('ccs_lock_close', 0)
             wago_write_modbus('ccs_lock_open', 1)
+            print("Schütze geöffnet")
             break
     return
 

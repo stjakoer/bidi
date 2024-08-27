b7ad476c1bb1b2a4de7c9a962255e20ae7c15f40
Jannick <koerja01@thu.de>
2024-08-27 17:10:29 +0200

    Auslesen

--- GUI_Bidi/evtec_bidi_test.py
+++ GUI_Bidi/evtec_bidi_test.py
@@ -13,4 +13,17 @@ low = input_power_value & 0xFFFF
 
 client.write_multiple_registers(600, [high, low])
 
+registers = client.read_holding_registers(register_address, 2)  # 2 Register lesen
+
 client.close()
+
+# Überprüfen, ob das Lesen erfolgreich war
+if registers:
+    # Die beiden 16-Bit-Werte zu einem 32-Bit-Wert kombinieren
+    high = registers[0]
+    low = registers[1]
+    input_power_value = (high << 16) + low
+
+    print(f"Wert des Registers 600: {input_power_value}")
+
+

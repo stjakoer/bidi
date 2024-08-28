451048695e3832777f4b091e0fbb9bd478e16411
Jannick <koerja01@thu.de>
2024-08-28 12:36:44 +0200

    big or lit

--- GUI_Bidi/evtec_bidi_test.py
+++ GUI_Bidi/evtec_bidi_test.py
@@ -4,14 +4,17 @@ import time
 client = ModbusClient(host="192.168.2.201", port=5020, unit_id=2)
 
 
-def evtec_schreiben(input_power_value):
+def evtec_schreiben(input_power_value,big_or_lit):
     client.open()
     # Aufteilen in High und Low 16-Bit-Werte
     high = (input_power_value >> 16) & 0xFFFF
     low = input_power_value & 0xFFFF
-
-  #  client.write_multiple_registers(600, [high, low]) # big endian
-    client.write_multiple_registers(600, [low, high]) # little endian
+    if big_or_lit == 'big':
+        if client.write_multiple_registers(600, [high, low]): # big endian
+            print("Big Endian erfolgreich geschrieben!")
+    elif big_or_lit == 'lit':
+        if client.write_multiple_registers(600, [low, high]):    # little endian
+            print("Little Endian erfolgreich geschrieben!")
     client.close()
 
 
@@ -22,10 +25,10 @@ def evtec_lesen():
     value_big = struct.unpack('>i', struct.pack('>HH', *regs))[0]
     value_little = struct.unpack('<i', struct.pack('<HH', *regs))[0]
 
-    print(f"Wert des Registers 600: {value_big}, {value_little}")
+    print(f"Big Endian: {value_big},Little Endian: {value_little}")
 
 def main():
-    evtec_schreiben(1000)
+    evtec_schreiben(1000,'big')
     time.sleep(4)
     evtec_lesen()
 

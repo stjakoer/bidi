18bd48e2374fe45f3eea06018c5d1ccbb17d4fc3
Jannick <koerja01@thu.de>
2024-08-28 11:48:38 +0200

    allles in funktionen organisiert

--- GUI_Bidi/evtec_bidi_test.py
+++ GUI_Bidi/evtec_bidi_test.py
@@ -1,29 +1,32 @@
 from pyModbusTCP.client import ModbusClient
-
-
+import struct
 client = ModbusClient(host="192.168.2.201", port=5020, unit_id=2)
 
-client.open()
 
-input_power_value = 100  # Beispielwert in Watt
+def evtec_schreiben(input_power_value):
+    client.open()
+    # Aufteilen in High und Low 16-Bit-Werte
+    high = (input_power_value >> 16) & 0xFFFF
+    low = input_power_value & 0xFFFF
 
-# Aufteilen in High und Low 16-Bit-Werte
-high = (input_power_value >> 16) & 0xFFFF
-low = input_power_value & 0xFFFF
+    client.write_multiple_registers(600, [high, low])
+    client.close()
 
-client.write_multiple_registers(600, [high, low])
 
-registers = client.read_holding_registers(600, 2)  # 2 Register lesen
+def evtec_lesen():
+    client.open()
+    regs = client.read_holding_registers(600, 2)  # 2 Register lesen
+    client.close()
+    value = struct.unpack('>f', struct.pack('>HH', *regs))[0]
 
-client.close()
+    print(f"Wert des Registers 600: {value}")
 
-# Überprüfen, ob das Lesen erfolgreich war
-if registers:
-    # Die beiden 16-Bit-Werte zu einem 32-Bit-Wert kombinieren
-    high = registers[0]
-    low = registers[1]
-    input_power_value = (high << 16) + low
+def main():
+    #evtec_schreiben(1000)
+    evtec_lesen()
 
-    print(f"Wert des Registers 600: {input_power_value}")
+
+if __name__ == "__main__":
+    main()
 
 
